2f5945dc48e41aee9a70457a327f8f7da30297ea
joshuaspiral <82312141+joshuaspiral@users.noreply.github.com>
2022-03-27 13:56:39 +0800

    back to 2d for better perf

--- worley.c
+++ worley.c
@@ -9,46 +9,43 @@
 double grid[SCREEN_HEIGHT * SCREEN_WIDTH];
 
 // euclidean distance
-int distance(int x1, int y1, int z1, int x2, int y2, int z2) {
-    return sqrt((double)pow(x1 - x2, 2) + pow(y1 - y2, 2) + pow(z1 - z2, 2));
+int distance(int x1, int y1, int x2, int y2) {
+    return sqrt((double)pow(x1 - x2, 2) + pow(y1 - y2, 2));
 }
 
-Vector3 featurePoints[100];
+Vector2 featurePoints[100];
 const int featurePointCount = 100;
 
-void updateGrid(int layer) {
+void updateGrid() {
     for (int i = 0; i < SCREEN_HEIGHT * SCREEN_WIDTH; i++) {
         int lowestDistance =
-            distance(i % SCREEN_WIDTH, i / SCREEN_WIDTH, layer, featurePoints[0].y,
-                    featurePoints[0].x, featurePoints[0].z);
+            distance(i % SCREEN_WIDTH, i / SCREEN_WIDTH, featurePoints[0].y,
+                    featurePoints[0].x);
         for (int j = 0; j < featurePointCount; j++) {
-            if (distance(i / SCREEN_WIDTH, i % SCREEN_WIDTH, layer,
-                        featurePoints[j].x, featurePoints[j].y,
-                        featurePoints[j].z) < lowestDistance)
-                lowestDistance = distance(i / SCREEN_WIDTH, i % SCREEN_WIDTH, layer,
-                        featurePoints[j].x, featurePoints[j].y,
-                        featurePoints[j].z);
+            if (distance(i / SCREEN_WIDTH, i % SCREEN_WIDTH,
+                        featurePoints[j].x, featurePoints[j].y) < lowestDistance)
+                lowestDistance = distance(i / SCREEN_WIDTH, i % SCREEN_WIDTH,
+                        featurePoints[j].x, featurePoints[j].y
+                        );
         }
         grid[i] = lowestDistance;
     }
 }
 
 int main() {
-    int layer = SCREEN_WIDTH / 2;
+    srand(time(0));
     for (int i = 0; i < featurePointCount; i++) {
         featurePoints[i].x = rand() % SCREEN_WIDTH;
         featurePoints[i].y = rand() % SCREEN_HEIGHT;
-        featurePoints[i].z = rand() % SCREEN_WIDTH;
     }
 
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Worley Noise Generator");
+    updateGrid();
     while (!WindowShouldClose()) {
-        layer+=5;
         BeginDrawing();
         ClearBackground(WHITE);
-        updateGrid(layer);
         for (int i = 0; i < SCREEN_HEIGHT * SCREEN_WIDTH; i++) {
-            Color color = {grid[i], grid[i], grid[i], 255};
+            Color color = {grid[i] , grid[i], grid[i], 255};
             DrawPixel(i / SCREEN_WIDTH, i % SCREEN_WIDTH, color);
         }
         EndDrawing();
